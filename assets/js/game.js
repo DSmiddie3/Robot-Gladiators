4c96fadd1cf2aaa4554434b20e0a5e7790e21da1
Dustin Smiddie <101125377+DSmiddie3@users.noreply.github.com>
2022-06-17 23:29:40 -0400

    Player Name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
-window.alert("this is an alert! JavaScript is running!");
+//window.alert("this is an alert! JavaScript is running!");
+
+// This creates a function named "fight"
+
+var playerName = window.prompt("What is your robot's name?");
+
+// Note the lack of quotation marks around playerName
+
+//window.alert(playerName);
+console.log(playerName);
+
+function fight() {
+    window.alert("The Fight Has Begun!")
+}
+
+//fight();
